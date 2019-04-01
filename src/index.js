4f437e5fc263a3995073105e1ef960cb043fca73
kikarto <kikarto@gmail.com>
2019-04-01 21:10:47 +0200

    sass

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import _ from './script.js';
-_();
+//import _ from './script.js';
+//_();
+
+//import './main.sass';
+
+document.body.innerHTML = '<h1>Hello!</h1> <p>I am styled.</p>';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const isProduction = process.env.NODE_ENV === 'production'; 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({         
+            filename: "[name].css",             
+            chunkFilename: "[id].css" 
+        })
+    ],
     module: {
         rules: [
             {
@@ -18,6 +26,17 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {             
+                test: /\.s(a|c)ss$/,             
+                use: [    
+                    isProduction                 
+                    ? MiniCssExtractPlugin.loader  
+                    : { loader: 'style-loader', options: { sourceMap: true } },
+                    { loader: 'css-loader', options: { sourceMap: isProduction } },
+                    { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+                    { loader: 'sass-loader', options: { sourceMap: isProduction } }           
+                ]    
             }
         ]
     }
