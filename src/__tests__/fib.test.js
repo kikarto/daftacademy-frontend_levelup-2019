b1967f33664be62fa79504967365a1403038cd25
kikarto <kikarto@gmail.com>
2019-04-27 18:22:57 +0200

    Update fib.test.js

--- src/__tests__/fib.test.js
+++ src/__tests__/fib.test.js
@@ -1,27 +1,39 @@
 
 const fib = require('../fib.js');
 
-it('czy istnieje funkcja fib', () => {
-    expect(typeof fib === 'function').toBe(true);
-});
+describe('Fibonacci test', ()=>{
 
-it('nie podano parametru', () => {
-    expect(fib()).toEqual([]);
-});
+    it('czy istnieje funkcja fib', () => {
+        expect(typeof fib === 'function').toBe(true);
+    });
 
-it('podano więcej parametrów', () => {
-    expect(fib(10, "ala", "ma", "kota")).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
-});
+    it('nie podano parametru', () => {
+        expect(fib()).toEqual([]);
+    });
 
-it('podano string, oczekiwany parametr musi być liczbą całkowitą', () => {
-    expect(fib('String param')).toEqual([]);
-});
+    it('podano więcej parametrów', () => {
+        expect(fib(10, "ala", "ma", "kota")).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+    });
 
-it('podano ujemną liczbę, oczekiwany parametr musi być liczbą większą od zera', () => {
-    expect(fib(-10)).toEqual([]);
-});
+    it('podano string, oczekiwany parametr musi być liczbą całkowitą', () => {
+        expect(fib('String param')).toEqual([]);
+    });
 
-it('prawidłowe wywołanie, oczekiwany wynik jest tablica z 20 pierwszymi liczbami ciągu Fibonacciego', () => {
-    expect(fib(20)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]);
+    it('podano więcej parametrów', () => {
+        expect(fib("ala", "ma", "kota")).toEqual([]);
+    });
+
+    it('podano ujemną liczbę, oczekiwany parametr musi być liczbą większą od zera', () => {
+        expect(fib(-10)).toEqual([]);
+    });
+
+    it('prawidłowe wywołanie, oczekiwany wynik jest tablica z 20 elementami', () => {
+        expect(fib(20).length === 20).toBe(true);
+    });
+
+    it('prawidłowe wywołanie, oczekiwany wynik jest tablica z 20 pierwszymi liczbami ciągu Fibonacciego', () => {
+        expect(fib(20)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]);
+    });
+
 });
 
